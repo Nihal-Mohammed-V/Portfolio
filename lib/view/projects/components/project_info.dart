0a16c0cea0aa78f7dcc09c84894e9b59705fee51
Nihal-Mohammed <nihalmhdv25@gmail.com>
2025-07-11 17:53:38 +0530

    Layout issues fixed

--- lib/view/projects/components/project_info.dart
+++ lib/view/projects/components/project_info.dart
@@ -10,25 +10,23 @@ class ProjectStack extends StatelessWidget {
   final controller = Get.put(ProjectController());
   ProjectStack({super.key, required this.index});
   final int index;
+
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onHover: (value) {
-        controller.onHover(index, value);
-      },
-      onTap: () {
-        ImageViewer(context, projectList[index].image);
-      },
+      onHover: (value) => controller.onHover(index, value),
+      onTap: () => ImageViewer(context, projectList[index].image),
       borderRadius: BorderRadius.circular(30),
-      child: AnimatedContainer(
-        padding: const EdgeInsets.only(
-            left: defaultPadding, right: defaultPadding, top: defaultPadding),
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(30), color: bgColor),
-        duration: const Duration(milliseconds: 500),
-        child: ProjectDetail(
-          index: index,
+      child: Container(
+        constraints: const BoxConstraints(
+          minHeight: 200, // Set a minimum height
+          maxHeight: double.infinity, // Allow expansion
         ),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(30),
+          color: bgColor,
+        ),
+        child: ProjectDetail(index: index),
       ),
     );
   }
